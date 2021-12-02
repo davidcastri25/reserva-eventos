3a14f6de3837dda23343345134682966a7ac1f99
David Castrillón <david.castri@hotmail.es>
2021-12-02 13:46:42 +0100

    Saco la shopping cart sin agrupar por evento

--- src/app/shared/components/shopping-cart/shopping-cart.component.ts
+++ src/app/shared/components/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cart } from 'src/app/events/interfaces/interfaces';
+import { CartService } from 'src/app/events/services/cart.service';
+
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  cartArray!: Cart[]; //Recibirá el array desde el cartService
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
+    this.cartService.cart$
+      .subscribe(cartArrayReturned => {
+        this.cartArray = cartArrayReturned;
+        console.log('shoppingCart', this.cartArray);
+      });
   }
-
 }
